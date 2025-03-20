1fca5f7d0d1a610de20b7a27f095e6e450c6ce26
BradyWok <bradywojnovich@berkeley.edu>
2025-03-20 14:19:30 -0700

    added special names and gifs

--- week6/Brady Wojnovich/index.js
+++ week6/Brady Wojnovich/index.js
@@ -3,23 +3,66 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+const axios = require('axios');
+
 const io = new Server(server);
+
+
+const GIPHY_API_KEY = 'XxUZfdq9Eg59ySml9jrXqb8jZ5rbNTyt';
+
+
+const nicknames = ["CoolPenguin", "FireDragon", "HappyCactus", "SpeedyTurtle", "NeonFox"];
+const users = {};
+
+
+const gifCategories = ["funny", "cat", "dog", "happy", "sad", "dance", "meme", "excited", "love", "wow"];
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  const userId = socket.id; 
-  console.log(`User connected: ${userId}`);
+  
+  const randomName = nicknames[Math.floor(Math.random() * nicknames.length)];
+  users[socket.id] = randomName;
+  console.log(`User connected: ${randomName} (${socket.id})`);
 
-  socket.on('chat message', (msg) => {
-    const messageData = { id: userId, text: msg };
-    io.emit('chat message', messageData); 
+  socket.on('chat message', async (msg) => {
+    
+    if (msg === '/gif list') {
+      socket.emit('chat message', {
+        type: 'list',
+        text: "Available GIF commands:\n" + gifCategories.map(g => `/gif ${g}`).join('\n')
+      });
+      return;
+    }
+
+    
+    if (msg.startsWith('/gif ')) {
+      const searchTerm = msg.replace('/gif ', '');
+      try {
+        const response = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${searchTerm}&limit=1`);
+        const gifUrl = response.data.data[0]?.images?.fixed_height?.url;
+
+        if (gifUrl) {
+          io.emit('chat message', { type: 'gif', name: users[socket.id], url: gifUrl });
+        } else {
+          socket.emit('chat message', { type: 'text', text: 'No GIF found! Try a different keyword.' });
+        }
+      } catch (error) {
+        console.error(error);
+        socket.emit('chat message', { type: 'text', text: 'Error fetching GIF. Try again later.' });
+      }
+      return;
+    }
+
+    
+    io.emit('chat message', { type: 'text', name: users[socket.id], text: msg });
   });
 
   socket.on('disconnect', () => {
-    console.log(`User disconnected: ${userId}`);
+    console.log(`User disconnected: ${users[socket.id]} (${socket.id})`);
+    delete users[socket.id]; 
   });
 });
 
